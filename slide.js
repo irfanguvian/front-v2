a66373286abe3763c2b9b01df9f0b7d53202fdae
irfanguvian <guvian14@gmail.com>
2021-06-30 17:17:53 +0700

    home page html

---
+++ slide.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function () {
+  sliderOne()
+  sliderTwo()
+})
+
+function sliderOne() {
+  let one = new Splide('.splide', {
+    type: 'loop',
+    padding: {
+      right: '5rem',
+      left: '5rem',
+    },
+  }).mount()
+}
+
+function sliderTwo() {
+  let second = new Splide('#splide', {
+    type: 'loop',
+    autoWidth: true,
+    focus: 'center',
+  }).mount()
+}
